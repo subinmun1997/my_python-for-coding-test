3c6788128cd737a813fa9b6ec6f1eb45615ded26
subinmun1997 <qzxy812@gmail.com>
2020-10-22 16:25:52 +0900

    Sort library

---
+++ Sort- sort library.py
@@ -0,0 +1,11 @@
+array = [7,5,9,0,3,1,6,2,4,8]
+array2 = (7,5,9,0,3,1,6,2,4,8)
+array3 = {7,5,9,0,3,1,6,2,4,8}
+
+result = sorted(array)
+result2 = sorted(array2)
+result3 = sorted(array3)
+
+print(result) #sorted함수는 집합 자료형이나 딕셔너리 자료형을 입력받아도 반환되는 결과는 리스트 자료형이다.
+print(result2)
+print(result3)
