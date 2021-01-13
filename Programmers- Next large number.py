168168abd4b18d56ea28e92eb476aeb945e067cb
subinmun1997 <qzxy812@gmail.com>
2021-01-13 21:52:58 +0900

    Programmers

---
+++ Programmers- Next large number.py
@@ -0,0 +1,9 @@
+def nextBigNumber(n):
+    num1 = bin(n).count('1')
+    while True:
+        n = n + 1
+        if num1 == bin(n).count('1'):
+            break
+    return n
+
+print(nextBigNumber(78))
