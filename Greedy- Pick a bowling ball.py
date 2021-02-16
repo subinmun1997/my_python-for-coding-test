8a6b18be5f9a3893795a066244803792ca450ff8
subinmun1997 <qzxy812@gmail.com>
2021-02-16 20:41:48 +0900

    Greedy

---
+++ Greedy- Pick a bowling ball.py
@@ -0,0 +1,11 @@
+n, m = map(int, input().split())
+k = list(map(int, input().split()))
+
+count = 0
+for i in range(n-1):
+    k[i]
+    for j in range(i+1, n):
+        if k[i] != k[j]:
+            count += 1
+
+print(count)
