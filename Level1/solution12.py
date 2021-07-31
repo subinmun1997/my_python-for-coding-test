9648909eb28fcb6c8dabd4f3c43dc04b2f1f1d35
subinmun1997 <qzxy812@gmail.com>
2021-07-31 16:45:46 +0900

    완주하지 못한 선수

---
+++ Level1/solution12.py
@@ -0,0 +1,9 @@
+def solution(participant, completion):
+    participant.sort()
+    completion.sort()
+
+    for a,b in zip(participant,completion):
+        if a != b:
+            return a
+
+    return participant.pop()
