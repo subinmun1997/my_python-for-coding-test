c1cbda4bc539f3e2a373c0def58bad69e2fbeb4c
subinmun1997 <qzxy812@gmail.com>
2021-08-07 00:24:49 +0900

    수박수박수박수박수박수?

--- Level1/solution34.py
+++ Level1/solution34.py
@@ -10,3 +10,15 @@ def solution(n):
 
 print(solution(3))
 print(solution(4))
+
+# 다른 코드1
+# def solution(n):
+#   s = '수박' * n
+#   return s[:n]
+
+
+
+# 다른 코드2
+# def solution(n):
+#   return "수박"*(n//2) + "수"*(n%2)
+#
