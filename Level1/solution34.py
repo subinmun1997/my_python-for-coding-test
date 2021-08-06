55c2ced1d8a07ff3c80bc720d9dbd79607ff907b
subinmun1997 <qzxy812@gmail.com>
2021-08-06 23:47:47 +0900

    수박수박수박수박수박수?

---
+++ Level1/solution34.py
@@ -0,0 +1,12 @@
+def solution(n):
+    answer = ''
+    for i in range(n):
+        if i%2 == 0:
+            answer += '수'
+        else:
+            answer += '박'
+
+    return answer
+
+print(solution(3))
+print(solution(4))
