0a1bea06b1513f7924bc9ff5c4422fd94926a195
subinmun1997 <qzxy812@gmail.com>
2021-08-07 14:51:39 +0900

    문자열을 정수로 바꾸기

---
+++ Level1/solution35.py
@@ -0,0 +1,14 @@
+def solution(s):
+    answer = s[0]
+    if answer == '+':
+        return int(s)
+    else:
+        return int(s)
+
+print(solution("1234"))
+print(solution("-1234"))
+
+# 한 줄 코드
+
+# def solution(s):
+#   return int(s)
