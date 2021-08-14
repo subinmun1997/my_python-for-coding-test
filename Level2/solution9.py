c868b0c6fb024df7c12111dd99534e68523f74f1
subinmun1997 <qzxy812@gmail.com>
2021-08-14 20:16:25 +0900

    숫자의 표현

---
+++ Level2/solution9.py
@@ -0,0 +1,14 @@
+def solution(n):
+    answer = 0
+    for i in range(1, n+1):
+        cnt = 0
+        for j in range(i,n+1):
+            cnt += j
+            if cnt == n:
+                answer += 1
+            elif cnt > n:
+                break
+
+    return answer
+
+print(solution(15))
