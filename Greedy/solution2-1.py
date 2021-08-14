f5a2d986a8c57a6453b3e0331a5086f8045e4602
subinmun1997 <qzxy812@gmail.com>
2021-08-14 22:11:31 +0900

    큰 수의 법칙(코드 수정)

---
+++ Greedy/solution2-1.py
@@ -0,0 +1,21 @@
+n, m, k = map(int, input().split())
+data = list(map(int, input().split()))
+
+data.sort()
+first = data[n-1]
+second = data[n-2]
+
+result = 0
+
+while True:
+    for i in range(k): # 가장 큰 수를 k번 더하기
+        if m == 0: # m이 0이라면 반복문 탈출
+            break
+        result += first
+        m -= 1 # 더할 때마다 1씩 빼기
+    if m == 0:
+        break
+    result += second
+    m -= 1
+
+print(result)
