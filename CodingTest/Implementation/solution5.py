8b74cab73918cf1c0fca363c6fa3e5cf952ac4c7
subinmun1997 <qzxy812@gmail.com>
2021-09-17 12:07:57 +0900

    럭키 스트레이트

---
+++ CodingTest/Implementation/solution5.py
@@ -0,0 +1,16 @@
+n = int(input())
+temp = str(n)
+
+result1 = 0
+result2 = 0
+mid =  len(temp) // 2
+for i in range(mid):
+    result1 += int(temp[i])
+
+for i in range(mid,len(temp)):
+    result2 += int(temp[i])
+
+if result1 == result2:
+    print("LUCKY")
+else:
+    print("READY")
