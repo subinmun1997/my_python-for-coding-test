c4e3f6ce31024f3d7743ea5cce83dbc6d130603b
subinmun1997 <qzxy812@gmail.com>
2021-09-18 16:05:43 +0900

    딕셔너리 자료형(1)

---
+++ Python/Chapter2/solution9.py
@@ -0,0 +1,45 @@
+# 딕셔너리 자료형
+
+dic = {'name':'pey','phone':'0119993323','birth':'1118'}
+print(dic)
+
+a = {1:'hi'}
+print(a)
+
+a = {'a':[1,2,3]}
+print(a)
+
+# 딕셔너리 쌍 추가, 삭제하기
+
+a = {1:'a'}
+a[2] = 'b'
+print(a)
+
+a['name'] = 'pey'
+print(a)
+
+a[3] = [1,2,3]
+print(a)
+
+# 딕셔너리 요소 삭제하기
+del a[1]
+print(a)
+
+# 딕셔너리에서 key 사용해 value 얻기
+grade = {'pey':10, 'julliet':99}
+print(grade['pey'])
+print(grade['julliet'])
+
+a = {1:'a',2:'b'}
+print(a[1])
+print(a[2])
+
+a = {'a':1,'b':2}
+print(a['a'])
+print(a['b'])
+
+dict = {'name':'pey','phone':'0109993323','birth':'1118'}
+print(dict['name'])
+print(dict['phone'])
+print(dict['birth'])
+
