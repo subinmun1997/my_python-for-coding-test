faad45e1595363d3e06e82696610547f265f6e42
subinmun1997 <qzxy812@gmail.com>
2021-09-18 10:40:11 +0900

    숫자형

---
+++ Python/Chapter2/solution1.py
@@ -0,0 +1,53 @@
+# 정수형
+a = 123
+print(a)
+
+a = -178
+print(a)
+
+a = 0
+print(a)
+
+# 실수형
+a = 1.2
+print(a)
+
+a = -3.45
+print(a)
+
+a = 4.24E10
+print(a)
+
+a = 4.24e-10
+print(a)
+
+# 8진수
+a = 0o177
+print(a)
+
+a = 0O177
+print(a)
+
+# 16진수
+a = 0X8ff
+print(a)
+
+a = 0XABC
+print(a)
+
+# 사칙연산
+a = 3
+b = 4
+print(a+b)
+print(a*b)
+print(a/b)
+
+# 제곱연산
+print(a**b)
+
+# 나머지 연산
+print(7%3)
+print(3%7)
+
+# 몫 반환
+print(7//4)
