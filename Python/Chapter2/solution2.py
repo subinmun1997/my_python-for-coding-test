d42c51482fe99384980d71c3bd5b3f77ed99f0c3
subinmun1997 <qzxy812@gmail.com>
2021-09-18 10:51:27 +0900

    문자열과 자료형(1)

---
+++ Python/Chapter2/solution2.py
@@ -0,0 +1,46 @@
+# 문자열
+print("Life is too short, you need python")
+print("a")
+print("!23")
+
+# 큰따옴표로 문자열
+print("Hello world")
+
+# 작은따옴표로 문자열
+print('Python is fun')
+
+# 큰따옴표 3개로 문자열
+print("""Life is too short, You need python""")
+
+# 작은따옴표 3개로 문자열
+print('''Life is too short, You need python''')
+
+# 문자열에 작은따옴표 포함
+print("Python's favorite food id perl")
+
+# 문자열에 큰따옴표 포함
+print('"Python is very easy" he says')
+
+# 백슬래시(\)를 이용해서 작은따옴표 큰따옴표 포함
+food = 'Python\'s favorite food is perl'
+print(food)
+
+say = "\"Python is very easy.\" he says"
+print(say)
+
+# 여러 줄인 문자열 대입
+# 줄을 바꾸는 이스케이프 코드 '\n' 삽입
+multiline = "Life is too short\nYou need python"
+print(multiline)
+
+# 연속된 작은따옴표 3개 또는 큰따옴표 3개 사용
+multiline = '''
+Life is too short
+You need python
+'''
+print(multiline)
+
+multiline = """
+Life is too short
+You need python
+"""
