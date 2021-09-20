1bacfc497d77b1c461e149e104447e6680b419c5
subinmun1997 <qzxy812@gmail.com>
2021-09-20 15:20:59 +0900

    x in s / x not in s

---
+++ Python/Chapter3/solution4.py
@@ -0,0 +1,9 @@
+# x in s / x not in s
+
+print(1 in [1,2,3])
+print(1 not in [1,2,3])
+print('a' in ('a','b','c'))
+print('j' not in 'python')
+print(1 in (1,2,3))
+print(1 in ('1','2','3'))
+print('p' in 'python')
