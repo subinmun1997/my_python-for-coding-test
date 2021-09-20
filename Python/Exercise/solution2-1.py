c1271f8bf72a3a14a459a4703d445ae6fa1526f3
subinmun1997 <qzxy812@gmail.com>
2021-09-20 14:25:49 +0900

    Exercise ch2

---
+++ Python/Exercise/solution2-1.py
@@ -0,0 +1,5 @@
+dict = {'국어':80,'영어':75,'수학':55}
+
+average = dict.values()
+average = sum(average) / 3
+print(average)
---
+++ Python/Exercise/solution2-2.py
@@ -0,0 +1,6 @@
+num = 13
+
+if num%2 == 0:
+    print("짝수")
+else:
+    print("홀수")
