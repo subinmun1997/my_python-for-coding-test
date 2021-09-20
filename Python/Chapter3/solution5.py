110918acdb1863a7d5be71ef5c1015f726255a31
subinmun1997 <qzxy812@gmail.com>
2021-09-20 15:23:57 +0900

    x in s / x not in s

---
+++ Python/Chapter3/solution5.py
@@ -0,0 +1,11 @@
+pocket = ['paper','cellphone','money']
+
+if 'money' in pocket:
+    print("택시를 타고 가라")
+else:
+    print("걸어 가라")
+
+if 'card' not in pocket:
+    print("걸어 가라")
+else:
+    print("버스를 타고 가라")
