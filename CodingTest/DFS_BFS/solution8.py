578ad0f1f8f6ffec84571c6c4b318da2e6d28817
subinmun1997 <qzxy812@gmail.com>
2021-09-24 19:41:10 +0900

    DFS

--- CodingTest/DFS_BFS/solution8.py
+++ CodingTest/DFS_BFS/solution8.py
@@ -15,15 +15,16 @@ DFS는 스택 자료구조를 이용하며 구체적인 동작 과정은 다음�
 '''
 
 def dfs(graph,v,visited):
-    visited[v] = True
+    visited[v] = True # 현재 노드를 방문 처리
     print(v, end=' ')
-
+    # 현재 노드와 연결된 다른 노드를 재귀적으로 방문
     for i in graph[v]:
         if not visited[i]:
             dfs(graph,i,visited)
 
+# 각 노드가 연결된 정보를 리스트 자료형으로 표현(2차원 리스트)
 graph = [
-    [],
+    [], # 그래프에선 노드의 번호가 1번부터 시작하는 경우가 많기 때문에 인덱스 '0'에 대한 내용은 비워두도록 한다.
     [2,3,8],
     [1,7],
     [1,4,5],
@@ -33,7 +34,7 @@ graph = [
     [2,6,8],
     [1,7]
 ]
-
+# 각 노드가 방문된 정보를 리스트 자료형으로 표현(1차원 리스트)
 visited = [False] * 9
-
+# 정의된 DFS 함수 호출
 dfs(graph,1,visited)
