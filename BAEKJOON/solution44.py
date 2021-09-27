0772c180600d2a598c23f4726ff7d2ca8c8712b1
subinmun1997 <qzxy812@gmail.com>
2021-09-27 19:03:52 +0900

    A+B (7)

--- BAEKJOON/solution44.py
+++ BAEKJOON/solution44.py
@@ -2,6 +2,4 @@ t = int(input())
 
 for i in range(t):
     a, b = map(int, input().split())
-    print("Case #",end='')
-    print(i,end='')
-    print(":",a+b)
+    print("Case #"+str(i+1)+":",a+b)
