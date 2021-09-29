3804d2bbfc6da9e2d1bba30db7fe518e9fdece66
subinmun1997 <qzxy812@gmail.com>
2021-09-29 15:02:56 +0900

    평균은 넘겠지

--- BAEKJOON/solution57.py
+++ BAEKJOON/solution57.py
@@ -1,14 +1,11 @@
 c = int(input())
 
 data = [list(map(int, input().split())) for _ in range(c)]
-print(data)
+
 for i in data:
     student = i[0]
-    print(student)
     score = i[1:]
-    print(score)
     avg = sum(score)/student
-    print(avg)
     good = [i for i in score if i > avg]
 
     print("{:.3f}".format(len(good)/student*100) + "%")
