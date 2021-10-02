a6c33e6246f4d0281985693cecf15c7e0bd45c4a
subinmun1997 <qzxy812@gmail.com>
2021-10-03 00:52:10 +0900

    소수

---
+++ BAEKJOON/solution72.py
@@ -0,0 +1,20 @@
+m = int(input())
+n = int(input())
+
+a = []
+
+for i in range(m, n+1):
+    is_sosu = False
+    for j in range(2,int(i**0.5)+1):
+        if i == 2:
+            a.append(i)
+        if i%j == 0:
+            is_sosu = False
+            break
+        else:
+            is_sosu = True
+            continue
+    if is_sosu:
+        a.append(i)
+
+print(sum(a), min(a), sep='\n') if len(a) > 0 else print(-1)
