205fae5ff332c3ae68fddb3a67292f05aa8f7005
subinmun1997 <qzxy812@gmail.com>
2021-10-03 13:38:56 +0900

    소수 구하기

---
+++ BAEKJOON/solution74.py
@@ -0,0 +1,13 @@
+def sosu(num):
+    if num == 1:
+        return False
+    else:
+        for i in range(2, int(num**0.5)+1):
+            if num%i == 0:
+                return False
+        return True
+
+m, n = map(int, input().split())
+for i in range(m, n+1):
+    if sosu(i):
+        print(i)
