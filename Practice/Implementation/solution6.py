06f3ee40da910a5eef7de376cce3b4638d007c88
subinmun1997 <qzxy812@gmail.com>
2021-10-25 23:33:47 +0900

    럭키 스트레이트

---
+++ Practice/Implementation/solution6.py
@@ -0,0 +1,16 @@
+n = input()
+length = len(n)
+
+sum1 = 0
+sum2 = 0
+
+for i in range(length//2):
+    sum1 += int(n[i])
+
+for i in range(length//2,length):
+    sum2 += int(n[i])
+
+if sum1 == sum2:
+    print("LUCKY")
+else:
+    print("READY")
