c6434c51a5aecc5e8bd6fe3c8aabf2a7147b84a1
subinmun1997 <qzxy812@gmail.com>
2021-10-26 00:36:29 +0900

    문자열 재정렬(2)

---
+++ Practice/Implementation/solution9.py
@@ -0,0 +1,17 @@
+data = input()
+result = []
+value = 0
+
+for x in data:
+    if x.isalpha():
+        result.append(x)
+    else:
+        value += int(x)
+
+result.sort()
+
+# 숫자가 하나라도 존재하는 경우 가장 뒤에 삽입
+if value != 0:
+    result.append(str(value))
+
+print(''.join(result))
