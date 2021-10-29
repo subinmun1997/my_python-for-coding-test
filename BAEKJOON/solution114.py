f3a76b14cd4872b847d24e0327008544185e325d
subinmun1997 <qzxy812@gmail.com>
2021-10-29 22:22:21 +0900

    보물

---
+++ BAEKJOON/solution114.py
@@ -0,0 +1,13 @@
+n = int(input())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+result = 0
+
+a.sort()
+b.sort(reverse=True)
+
+for i in range(len(a)):
+    result += a[i] * b[i]
+
+print(result)
