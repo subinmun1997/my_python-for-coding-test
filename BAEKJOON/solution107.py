df10f56a31ef870d764fd96c1e8fc1d4063c9f99
subinmun1997 <qzxy812@gmail.com>
2021-10-29 14:54:19 +0900

    통계학

--- BAEKJOON/solution107.py
+++ BAEKJOON/solution107.py
@@ -1,13 +1,26 @@
-E, S, M = map(int, input().split())
+import sys
+from collections import Counter
+n = int(sys.stdin.readline())
+array = []
 
-def year(E, S, M):
-    e, s, m = 1, 1, 1
-    for i in range(15*28*19):
-        if e == E and s == S and m == M:
-            return i+1
-        e = e + 1 if e < 15 else 1
-        s = s + 1 if s < 28 else 1
-        m = m + 1 if m < 19 else 1
+for i in range(n):
+    array.append(int(sys.stdin.readline()))
 
-result = year(E, S, M)
-print(result)
+array.sort()
+print(round(sum(array)//n)) # 산술평균
+
+print(array[len(array)//2]) # 중앙값
+
+
+def mode(array):
+    counter = Counter(array)
+    if len(counter) == 1:
+        return array[0]
+    counting_arr = counter.most_common(n=2)
+    if counting_arr[0][1] == counting_arr[1][1]:
+        return counting_arr[1][0]
+    return counting_arr[0][0]
+
+print(mode(array)) # 최빈값
+
+print(array[-1] - array[0]) # 범위
