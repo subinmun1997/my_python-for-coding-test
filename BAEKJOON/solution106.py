2d3fde097e004e8f38600180b4b6ba0f14b26e8d
subinmun1997 <qzxy812@gmail.com>
2021-10-29 14:40:55 +0900

    날짜 계산

--- BAEKJOON/solution106.py
+++ BAEKJOON/solution106.py
@@ -1,1 +1,13 @@
-print("Test1")
+E, S, M = map(int, input().split())
+
+def year(E, S, M):
+    e, s, m = 1, 1, 1
+    for i in range(15*28*19):
+        if e == E and s == S and m == M:
+            return i+1
+        e = e + 1 if e < 15 else 1
+        s = s + 1 if s < 28 else 1
+        m = m + 1 if m < 19 else 1
+
+result = year(E, S, M)
+print(result)
