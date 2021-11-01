63c0fa06288ef87488db0acab42d399ab86a8bd8
subinmun1997 <qzxy812@gmail.com>
2021-11-02 01:19:31 +0900

    수 정렬하기 3

---
+++ BAEKJOON/solution123.py
@@ -0,0 +1,12 @@
+import sys
+test_num = int(sys.stdin.readline())
+
+num_list = [0] * 10001
+for _ in range(test_num):
+    data = int(sys.stdin.readline())
+    num_list[data] += 1
+
+for i in range(1, 10001):
+    if num_list[i] != 0:
+        for _ in range(num_list[i]):
+            print(i)
