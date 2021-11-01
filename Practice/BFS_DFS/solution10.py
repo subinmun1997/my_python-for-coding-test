4f21fe18f04fa5fd833fb569820e938e42eb48c2
subinmun1997 <qzxy812@gmail.com>
2021-11-01 18:31:14 +0900

    음료수 얼려 먹기

---
+++ Practice/BFS_DFS/solution10.py
@@ -0,0 +1,22 @@
+n, m = map(int, input().split())
+graph = [list(map(int, input())) for _ in range(n)]
+
+def dfs(x, y):
+    if x <= -1 or x >= n or y <= -1 or y >= m:
+        return False
+    if graph[x][y] == 0:
+        graph[x][y] = 1
+        dfs(x-1, y)
+        dfs(x, y-1)
+        dfs(x+1, y)
+        dfs(x, y+1)
+        return True
+    return False
+
+result = 0
+for i in range(n):
+    for j in range(m):
+        if dfs(i,j) == True:
+            result += 1
+
+print(result)
