f75399b9a1453c22c9586d855a589cea073a9996
subinmun1997 <qzxy812@gmail.com>
2021-11-03 18:35:12 +0900

    신입 사원

--- BAEKJOON/solution130.py
+++ BAEKJOON/solution130.py
@@ -9,7 +9,6 @@ for i in range(0, t):
     a = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
     a.sort()
     m = a[0][1]
-    print(m)
 
     for i in range(1, n):
         if m > a[i][1]:
