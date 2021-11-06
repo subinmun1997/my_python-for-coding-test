5786643778fa95e9cdc20dfb4b5d65e8bb9d4ebf
subinmun1997 <qzxy812@gmail.com>
2021-11-06 11:37:19 +0900

    부품 찾기(2)

---
+++ Practice/BinarySearch/solution5.py
@@ -0,0 +1,25 @@
+import sys
+
+def binary_search(array, target, start, end):
+    if start > end:
+        return None
+    mid = (start + end) // 2
+    if array[mid] == target:
+        return mid
+    elif array[mid] > target:
+        return binary_search(array, target, start, mid-1)
+    else:
+        return binary_search(array, target, mid+1, end)
+
+n = int(sys.stdin.readline())
+items = list(map(int, sys.stdin.readline().split()))
+items.sort()
+m = int(sys.stdin.readline())
+data = list(map(int, sys.stdin.readline().split()))
+
+for i in data:
+    result = binary_search(items, i, 0, n-1)
+    if result == None:
+        print("no", end=' ')
+    else:
+        print("yes", end=' ')
