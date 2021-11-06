76f34b603b64d9f890970507ed210a300bcab4f6
subinmun1997 <qzxy812@gmail.com>
2021-11-06 13:11:21 +0900

    수 찾기

---
+++ BAEKJOON/solution140.py
@@ -0,0 +1,21 @@
+def binary_search(array, target, start, end):
+    if start > end:
+        return None
+    mid = (start + end) // 2
+    if array[mid] == target:
+        return mid
+    elif array[mid] > target:
+        return binary_search(array, target, start, mid-1)
+    else:
+        return binary_search(array, target, mid+1, end)
+
+n = int(input())
+array = set(map(int, input().split()))
+m = int(input())
+data = list(map(int, input().split()))
+
+for i in data:
+    if i in array:
+        print(1)
+    else:
+        print(0)
