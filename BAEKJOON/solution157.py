f34e9e1674084206f028de3eb076ee75d3e1f325
subinmun1997 <qzxy812@gmail.com>
2021-11-10 17:54:41 +0900

    평범한 배낭

--- BAEKJOON/solution157.py
+++ BAEKJOON/solution157.py
@@ -1,5 +1,6 @@
 n, k = map(int, input().split())
 dp = [[0 for _ in range(k+1)] for _ in range(n+1)]
+
 w = [0]
 v = [0]
 
@@ -12,6 +13,7 @@ for i in range(1, n+1):
     for j in range(1, k+1):
         weight = w[i]
         value = v[i]
+
         if j < weight:
             dp[i][j] = dp[i-1][j]
         else:
