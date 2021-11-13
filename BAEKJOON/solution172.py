338fc62425edd10d832e3f18fa45faff5e6e57bd
subinmun1997 <qzxy812@gmail.com>
2021-11-14 01:26:14 +0900

    국영수

--- BAEKJOON/solution172.py
+++ BAEKJOON/solution172.py
@@ -5,7 +5,7 @@ array = []
 for i in range(n):
     array.append(list(map(str, sys.stdin.readline().rstrip().split())))
 
-result = sorted(array, key=lambda x : (int(x[1])*(-1),int(x[2]),int(x[3])*(-1), x[0]))
+result = sorted(array, key=lambda x : (-int(x[1]),int(x[2]),(-int(x[3])), x[0]))
 
 for i in result:
     print(i[0])
