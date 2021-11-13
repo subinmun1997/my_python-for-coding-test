a6e6163ba2567c4a4ce8e4b1c825afbcbd9c401a
subinmun1997 <qzxy812@gmail.com>
2021-11-14 01:45:31 +0900

    배열 합치기

---
+++ BAEKJOON/solution174.py
@@ -0,0 +1,9 @@
+import sys
+
+n, m = map(int, sys.stdin.readline().split())
+a = list(map(int, sys.stdin.readline().split()))
+b = list(map(int, sys.stdin.readline().split()))
+
+c = sorted(a+b)
+for i in c:
+    print(i, end=' ')
