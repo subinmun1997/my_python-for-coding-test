c6f7e2ebc0dc3f78afcf5869e01fda752c6b68e6
subinmun1997 <qzxy812@gmail.com>
2021-11-14 01:23:58 +0900

    국영수

---
+++ BAEKJOON/solution172.py
@@ -0,0 +1,11 @@
+import sys
+n = int(sys.stdin.readline())
+
+array = []
+for i in range(n):
+    array.append(list(map(str, sys.stdin.readline().rstrip().split())))
+
+result = sorted(array, key=lambda x : (int(x[1])*(-1),int(x[2]),int(x[3])*(-1), x[0]))
+
+for i in result:
+    print(i[0])
