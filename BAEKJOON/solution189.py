5a912e1ff416ffe893c8d0a21d29e55b67966469
subinmun1997 <qzxy812@gmail.com>
2021-11-20 23:21:44 +0900

    럭키 스트레이트

---
+++ BAEKJOON/solution189.py
@@ -0,0 +1,14 @@
+n = input()
+
+left = 0
+for i in range(len(n)//2):
+    left += int(n[i])
+
+right = 0
+for i in range(len(n)//2,len(n)):
+    right += int(n[i])
+
+if left == right:
+    print("LUCKY")
+else:
+    print("READY")
