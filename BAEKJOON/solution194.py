a1795fc8ccc5354cab6cbf634067678f6b3477aa
subinmun1997 <qzxy812@gmail.com>
2021-11-22 13:23:26 +0900

    하얀 칸

---
+++ BAEKJOON/solution194.py
@@ -0,0 +1,15 @@
+graph = [input() for _ in range(8)]
+
+cnt = 0
+for i in range(8):
+    for j in range(8):
+        if i%2==0:
+            if j%2==0:
+                if graph[i][j] == 'F':
+                    cnt += 1
+        else:
+            if j%2!=0:
+                if graph[i][j] == 'F':
+                    cnt += 1
+
+print(cnt)
