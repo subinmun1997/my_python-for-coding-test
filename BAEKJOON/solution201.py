437e35e2440116bb749e0fad22e011c92d9811bb
subinmun1997 <qzxy812@gmail.com>
2021-11-23 11:05:43 +0900

    터렛

--- BAEKJOON/solution201.py
+++ BAEKJOON/solution201.py
@@ -4,7 +4,7 @@ t = int(input())
 
 for _ in range(t):
     x1,y1,r1,x2,y2,r2 = map(int, input().split())
-    d = math.dist((x1,y1),(x2,y2))
+    d = ((x1-x2)**2 + (y1-y2)**2) ** (1/2)
     if d == 0 and r1==r2: # 두 원이 일치할 때
         print(-1)
     elif d > (r1+r2) or d < abs(r1-r2): # 두 원이 안 만남
