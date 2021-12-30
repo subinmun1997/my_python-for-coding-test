d36a9566e9fd7f4b9e02b5d9d4fb4280e1512499
subinmun1997 <qzxy812@gmail.com>
2021-12-30 12:22:37 +0900

    solution13

---
+++ prepare_codingtest/solution13.py
@@ -0,0 +1,18 @@
+s = input()
+
+count0 = 0 # 0으로 바꾸기
+count1 = 0 # 1로 바꾸기
+
+if s[0] == '0':
+    count1 += 1
+else:
+    count0 += 1
+
+for i in range(1, len(s)):
+    if s[i-1] != s[i]:
+        if s[i] == '0':
+            count1 += 1
+        else:
+            count0 += 1
+
+print(min(count1, count0))
