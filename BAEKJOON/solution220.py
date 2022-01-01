cf301c3a91ffac6fd08122e9dd97e4701e07209e
subinmun1997 <qzxy812@gmail.com>
2022-01-01 17:51:35 +0900

    보석 도둑

---
+++ BAEKJOON/solution220.py
@@ -0,0 +1,27 @@
+import sys
+import heapq
+
+input = sys.stdin.readline
+
+N, K = map(int, input().split())
+jewel = []
+bag = []
+for _ in range(N):
+    heapq.heappush(jewel, list(map(int, input().split())))
+for _ in range(K):
+    bag.append(int(input()))
+
+bag.sort()
+
+result = 0
+candidate = []
+for b in bag:
+    while jewel and b >= jewel[0][0]:
+        w, v = heapq.heappop(jewel)
+        heapq.heappush(candidate, -v)
+    print(candidate)
+    if candidate:
+        result -= heapq.heappop(candidate)
+        print(result)
+
+print(result)
