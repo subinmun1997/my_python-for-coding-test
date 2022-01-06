acc09946b25714aeb505b40356a8239dcee5d2f7
subinmun1997 <qzxy812@gmail.com>
2022-01-06 16:14:46 +0900

    절댓값 힙

---
+++ BAEKJOON/solution236.py
@@ -0,0 +1,19 @@
+import heapq
+import sys
+
+input = sys.stdin.readline
+
+heap = []
+n = int(input())
+
+for _ in range(n):
+    x = int(input())
+    if x > 0 or x < 0:
+        heapq.heappush(heap,(abs(x),x))
+    elif x == 0:
+        if len(heap) != 0:
+            k = heapq.heappop(heap)[1]
+            print(k)
+        else:
+            print(0)
+
