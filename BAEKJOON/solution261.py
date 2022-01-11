f7b018e1e493400e450f73b48fe1168960382616
subinmun1997 <qzxy812@gmail.com>
2022-01-11 14:22:20 +0900

    알고리즘 수업 - 선택 정렬 1

--- BAEKJOON/solution261.py
+++ BAEKJOON/solution261.py
@@ -1,25 +1,24 @@
+import sys
+input = sys.stdin.readline
+
 n, k = map(int, input().split())
 data = list(map(int, input().split()))
 
 count = 0
-for i in range(len(data)):
-    min_index = i
+for i in range(n-1,0,-1):
     flag = 0
-    for j in range(i+1, len(data)):
-        if data[min_index] > data[j]:
-            min_index = j
+    max_idx = i
+    for j in range(i-1,-1,-1):
+        if data[max_idx] < data[j]:
+            max_idx = j
             flag = 1
     if flag == 1:
-        data[i], data[min_index] = data[min_index], data[i]
+        data[i], data[max_idx] = data[max_idx], data[i]
         count += 1
-        if count == k:
-            print(data[i], data[min_index])
         flag = 0
+        if count == k:
+            print(data[max_idx],data[i])
 
 if count < k:
     print(-1)
-
-
-
-
 
