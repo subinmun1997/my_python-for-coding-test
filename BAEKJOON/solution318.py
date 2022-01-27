a65f426c40e8abda9c26cda30226ebd2438a805b
subinmun1997 <qzxy812@gmail.com>
2022-01-27 15:25:29 +0900

    정수 삼각형

--- BAEKJOON/solution318.py
+++ BAEKJOON/solution318.py
@@ -1,6 +1,16 @@
 n = int(input())
 
-array = []
+t_list = []
 for _ in range(n):
-    t, p = map(int, input().split())
-    array.append((t, p))
+    t_list.append(list(map(int, input().split())))
+
+for i in range(1, n):
+    for j in range(i+1):
+        if j == 0:
+            t_list[i][j] += t_list[i-1][j]
+        elif j == i:
+            t_list[i][j] += t_list[i-1][j-1]
+        else:
+            t_list[i][j] = max(t_list[i][j] + t_list[i-1][j], t_list[i][j] + t_list[i-1][j-1])
+
+print(max(t_list[n-1]))
