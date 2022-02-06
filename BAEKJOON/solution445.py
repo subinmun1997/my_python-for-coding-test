6fcc12174242dece7c853e0663089d7386b28764
subinmun1997 <qzxy812@gmail.com>
2022-02-07 00:59:27 +0900

    나는야 포켓몬 마스터 이다솜

---
+++ BAEKJOON/solution445.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+pocketmon_names = []
+pocketmon_list = {}
+
+for i in range(1, n+1):
+    x = input().rstrip()
+    pocketmon_names.append(x)
+    pocketmon_list[x] = i
+
+for i in range(m):
+    x = input().rstrip()
+
+    if x.isdigit():
+        print(pocketmon_names[int(x)-1])
+    else:
+        print(pocketmon_list[x])
