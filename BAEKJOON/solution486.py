52e173eb30757373211cb0ae427e3aae59867547
subinmun1997 <qzxy812@gmail.com>
2022-02-11 22:36:47 +0900

    바구니 뒤집기

---
+++ BAEKJOON/solution486.py
@@ -0,0 +1,9 @@
+n, m = map(int, input().split())
+basket = [i for i in range(1, n+1)]
+
+for _ in range(m):
+    i,j = map(int, input().split())
+    basket[i-1:j] = basket[i-1:j][::-1]
+
+for b in basket:
+    print(b, end=' ')
