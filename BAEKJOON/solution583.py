ddcc17491160a76098644c704a0bc4530d49641b
subinmun1997 <qzxy812@gmail.com>
2022-03-03 21:54:13 +0900

    비트 우정지수

---
+++ BAEKJOON/solution583.py
@@ -0,0 +1,15 @@
+t = int(input())
+
+for _ in range(t):
+    n, m = map(str, input().split())
+
+    count0 = 0
+    count1 = 0
+    for i in range(len(n)):
+        if n[i] != m[i]:
+            if n[i] == '0':
+                count1 += 1
+            else:
+                count0 += 1
+
+    print(max(count0, count1))
