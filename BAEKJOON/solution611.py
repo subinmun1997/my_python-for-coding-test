33614f12c38d4bdaacfd5c11b89d3731f3316da6
subinmun1997 <qzxy812@gmail.com>
2022-03-12 15:12:30 +0900

    빙고

---
+++ BAEKJOON/solution611.py
@@ -0,0 +1,50 @@
+def check(arr):
+    cnt = 0
+    # 가로
+    for i in arr:
+        if i.count(0) == 5:
+            cnt += 1
+    # 세로
+    for i in range(5):
+        y = 0
+        for j in range(5):
+            if arr[j][i] == 0:
+                y += 1
+        if y == 5:
+            cnt += 1
+    # 대각선(\)
+    k = 0
+    for i in range(5):
+        if arr[i][i] == 0:
+            k += 1
+    if k == 5:
+        cnt += 1
+    # 대각선(/)
+    u = 0
+    for i in range(5):
+        if arr[i][4-i] == 0:
+            u += 1
+    if u == 5:
+        cnt += 1
+
+    return cnt
+
+
+a = [] # 빙고판
+b = [] # 부르는 숫자
+for _ in range(5):
+    a.append(list(map(int, input().split())))
+for i in range(5):
+    w = list(map(int, input().split()))
+    for j in w:
+        b.append(j)
+
+for idx, num in enumerate(b):
+    for m in a:
+        if num in m:
+            m[m.index(num)] = 0
+            break
+    res = check(a)
+    if res >= 3:
+        print(idx+1)
+        break
