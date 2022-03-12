e285e856530f092849abc6ca01fff76394391660
subinmun1997 <qzxy812@gmail.com>
2022-03-12 14:19:09 +0900

    오리

---
+++ BAEKJOON/solution610.py
@@ -0,0 +1,31 @@
+duck = input()
+word = 'quack'
+visited = [False] * len(duck)
+
+count = 0
+idx = 0
+
+if len(duck)%5 != 0 or len(duck) < 5:
+    print(-1)
+    exit()
+
+for i in range(len(duck)):
+    if duck[i] == 'q' and not visited[i]:
+        flag = True
+
+        for j in range(i, len(duck)):
+            if duck[j] == word[idx] and not visited[j]:
+                visited[j] = True
+
+                if duck[j] == 'k':
+                    if flag:
+                        count += 1
+                        flag = False
+                    idx = 0
+                    continue
+                idx += 1
+
+if not all(visited) or count == 0:
+    print(-1)
+else:
+    print(count)
