93b288d9ccbc447140f69e5fc07d5c707e9ea4fd
subinmun1997 <qzxy812@gmail.com>
2022-03-23 23:27:47 +0900

    택배

--- BAEKJOON/solution635.py
+++ BAEKJOON/solution635.py
@@ -6,14 +6,11 @@ truck.sort(key=lambda x : x[1])
 
 box = [c] * (n+1)
 answer = 0
-for s, e, b in truck:
-    _min = c
-    for i in range(s, e):
-        _min = min(_min, box[i])
-    _min = min(_min, b)
-    for i in range(s, e):
-        box[i] -= _min
 
-    answer += _min
+for s, r, b in truck:
+    min_box = min(min(truck[s:r]), b)
+    for i in range(s, r):
+        box[i] -= min_box
+    answer += min_box
 
 print(answer)
