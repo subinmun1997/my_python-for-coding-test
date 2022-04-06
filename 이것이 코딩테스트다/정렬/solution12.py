737a2649b325b5ce9c303ec46f3743cca95be559
subinmun1997 <qzxy812@gmail.com>
2022-04-07 01:27:31 +0900

    성적이 낮은 순서로 학생 출력하기

--- 이것이 코딩테스트다/정렬/solution12.py
+++ 이것이 코딩테스트다/정렬/solution12.py
@@ -5,6 +5,7 @@ for _ in range(n):
     data = input().split()
     array.append((data[0], int(data[1])))
 
+# 키(Key)를 이용하여 점수를 기준으로 정렬
 array.sort(key=lambda x : x[1])
 for i in array:
     print(i[0], end=' ')
