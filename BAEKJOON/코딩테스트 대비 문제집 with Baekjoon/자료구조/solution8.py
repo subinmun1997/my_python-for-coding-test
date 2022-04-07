fd8f586df638c09e3bdad5f3e2312e35b67f2e63
subinmun1997 <qzxy812@gmail.com>
2022-04-08 00:42:15 +0900

    후위 표기식2

---
+++ BAEKJOON/코딩테스트 대비 문제집 with Baekjoon/자료구조/solution8.py
@@ -0,0 +1,29 @@
+n = int(input())
+postfix = list(input())
+op = {}
+numbers = [int(input()) for _ in range(n)]
+for i in range(n):
+    op[chr(65+i)] = numbers[i]
+
+stack = []
+for i in postfix:
+    if i.isalpha():
+        stack.append(op[i])
+    elif i == '*':
+        a = stack.pop()
+        b = stack.pop()
+        stack.append(a*b)
+    elif i == "+":
+        a = stack.pop()
+        b = stack.pop()
+        stack.append(a+b)
+    elif i == '/':
+        a = stack.pop()
+        b = stack.pop()
+        stack.append(b/a)
+    elif i == '-':
+        a = stack.pop()
+        b = stack.pop()
+        stack.append(b-a)
+
+print(f"{sum(stack):.2f}")
