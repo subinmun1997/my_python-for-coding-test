179333ecefbccd5f53cc8d94900d67ff4e8bdbaa
subinmun1997 <qzxy812@gmail.com>
2022-04-09 01:10:10 +0900

    DFS와 BFS

---
+++ BAEKJOON/코딩테스트 대비 문제집 with Baekjoon/그래프 탐색/solution3.py
@@ -0,0 +1,36 @@
+from collections import deque
+
+n, m, v = map(int, input().split())
+graph = [[] for _ in range(n+1)]
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+for j in range(n+1):
+    graph[j].sort()
+
+def dfs(graph, v, visited):
+    visited[v] = True
+    print(v, end=' ')
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
+
+def bfs(graph, start, visited):
+    queue = deque([start])
+    visited[start] = True
+    while queue:
+        v = queue.popleft()
+        print(v, end=' ')
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
+
+visited = [False] * (n+1)
+dfs(graph, v, visited)
+print()
+
+visited = [False] * (n+1)
+bfs(graph, v, visited)
