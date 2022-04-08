b4c30e23bc00f86820efcb88875c4aecac698504
subinmun1997 <qzxy812@gmail.com>
2022-04-08 17:21:59 +0900

    복호화_다른 풀이

---
+++ BAEKJOON/코딩테스트 대비 문제집 with Baekjoon/문자열/solution5.py
@@ -0,0 +1,13 @@
+t = int(input())
+
+for _ in range(t):
+    s = input().replace(' ', '')
+    alpha = [0] * 26
+
+    for i in s:
+        alpha[ord(i) - 97] += 1
+
+    if alpha.count(max(alpha)) > 1:
+        print("?")
+    else:
+        print(chr(97 + alpha.index(max(alpha))))
