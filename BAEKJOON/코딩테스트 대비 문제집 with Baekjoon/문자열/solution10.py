33453a97db0c7aa36e0de9ada0d658adaa944f44
subinmun1997 <qzxy812@gmail.com>
2022-04-10 02:49:08 +0900

    비밀번호 발음하기

--- BAEKJOON/코딩테스트 대비 문제집 with Baekjoon/문자열/solution10.py
+++ BAEKJOON/코딩테스트 대비 문제집 with Baekjoon/문자열/solution10.py
@@ -10,21 +10,22 @@ while True:
         if s[i] in vows:
             count_v = 1
         if i > 0:
-            if s[i] == s[i-1] and s[i] != 'e' and s[i] != 'o':
+            if s[i] == s[i - 1] and s[i] != 'e' and s[i] != 'o':
                 flag = False
                 break
         if i > 1:
             if s[i] not in vows:
-                if s[i-1] not in vows:
-                    if s[i-2] not in vows:
+                if s[i - 1] not in vows:
+                    if s[i - 2] not in vows:
                         flag = False
                         break
             if s[i] in vows:
-                if s[i-1] in vows:
-                    if s[i-2] in vows:
+                if s[i - 1] in vows:
+                    if s[i - 2] in vows:
                         flag = False
                         break
-    if count_v >= 1 and flag:
+
+    if count_v == 1 and flag:
         print("<" + s + "> is acceptable.")
     else:
         print("<" + s + "> is not acceptable.")
