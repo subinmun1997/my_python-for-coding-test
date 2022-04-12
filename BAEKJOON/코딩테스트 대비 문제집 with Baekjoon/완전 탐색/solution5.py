dd6cdc8c30ca33d2613d33e05cd635529e902bb8
subinmun1997 <qzxy812@gmail.com>
2022-04-12 14:13:43 +0900

    시각

--- BAEKJOON/코딩테스트 대비 문제집 with Baekjoon/완전 탐색/solution5.py
+++ BAEKJOON/코딩테스트 대비 문제집 with Baekjoon/완전 탐색/solution5.py
@@ -2,9 +2,15 @@ n, k = map(int, input().split())
 
 result = 0
 for i in range(n+1):
+    if i < 10:
+        i = '0' + str(i)
     for j in range(60):
+        if j < 10:
+            j = '0' + str(j)
         for s in range(60):
-            if str(k) in str(i)+str(j)+str(s):
+            if s < 10:
+                s = '0' + str(s)
+            if str(k) in str(i) + str(j) + str(s):
                 result += 1
 
 print(result)
