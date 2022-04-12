5d9055e9a3ba5a35f6e5351902876d1ee46d518e
subinmun1997 <qzxy812@gmail.com>
2022-04-12 21:48:35 +0900

    큐

---
+++ BAEKJOON/코딩테스트 대비 문제집 with Baekjoon/자료구조/solution17.py
@@ -0,0 +1,33 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+n = int(input())
+queue = deque()
+
+for _ in range(n):
+    data = input().split()
+    if data[0] == "push":
+        queue.append(data[1])
+    elif data[0] == "pop":
+        if queue:
+            print(queue.popleft())
+        else:
+            print(-1)
+    elif data[0] == "size":
+        print(len(queue))
+    elif data[0] == "empty":
+        if queue:
+            print(0)
+        else:
+            print(1)
+    elif data[0] == "front":
+        if queue:
+            print(queue[0])
+        else:
+            print(-1)
+    elif data[0] == "back":
+        if queue:
+            print(queue[-1])
+        else:
+            print(-1)
