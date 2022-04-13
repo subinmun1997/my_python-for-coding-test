3b1365ba4105eb913392348dc6df32b2d08eae0a
subinmun1997 <qzxy812@gmail.com>
2022-04-14 00:27:42 +0900

    N과 M (4)

---
+++ BAEKJOON/코딩테스트 대비 문제집 with Baekjoon/백트래킹/solution4.py
@@ -0,0 +1,8 @@
+from itertools import combinations_with_replacement
+
+n, m = map(int, input().split())
+numbers = [i for i in range(1, n+1)]
+result = combinations_with_replacement(numbers, m)
+
+for i in result:
+    print(*i)
