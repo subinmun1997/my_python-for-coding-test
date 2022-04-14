aaf08c647324973efe529e1620d43e7dde4c1df9
subinmun1997 <qzxy812@gmail.com>
2022-04-15 01:12:47 +0900

    소수 찾기

--- BAEKJOON/코딩테스트 대비 문제집 with Baekjoon/수학/solution9.py
+++ BAEKJOON/코딩테스트 대비 문제집 with Baekjoon/수학/solution9.py
@@ -20,3 +20,21 @@ for number in numbers:
         result += 1
 
 print(result)
+
+'''
+# 더 좋은 방법
+n = int(input())
+numbers = list(map(int, input().split()))
+
+for number in numbers:
+    if number == 1:
+        n -= 1
+        continue
+
+    for i in range(2, int(number**0.5)+1):
+        if number%i == 0:
+            n -= 1
+            break
+
+print(n)
+'''
