3a6f7d468f2d23adba585e04c5141f0f5f72f8bd
subinmun1997 <qzxy812@gmail.com>
2022-04-15 00:59:01 +0900

    소인수분해

---
+++ BAEKJOON/코딩테스트 대비 문제집 with Baekjoon/수학/solution9.py
@@ -0,0 +1,23 @@
+def check(num):
+    count = 0
+
+    if num == 1:
+        return False
+    for i in range(2, num+1):
+        if num%i == 0:
+            count += 1
+
+    if count > 1:
+        return False
+    else:
+        return True
+
+n = int(input())
+numbers = list(map(int, input().split()))
+
+result = 0
+for number in numbers:
+    if check(number):
+        result += 1
+
+print(result)
