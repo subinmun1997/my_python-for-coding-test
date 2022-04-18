7910c3c3210bff269c263346815920c07323a1bb
subinmun1997 <qzxy812@gmail.com>
2022-04-18 21:15:41 +0900

    회전하는 큐_다른 풀이

---
+++ BAEKJOON/코딩테스트 대비 문제집 with Baekjoon/자료구조/solution21.py
@@ -0,0 +1,29 @@
+n, m = map(int, input().split())
+numbers = list(map(int, input().split()))
+
+queue = [i for i in range(1, n+1)]
+result = 0
+
+for i in range(m):
+    queue_len = len(queue)
+    idx = queue.index(numbers[i])
+
+    if idx < queue_len - idx:
+        while True:
+            if queue[0] == numbers[i]:
+                queue.pop(0)
+                break
+            else:
+                queue.append(queue.pop(0))
+                result += 1
+
+    else:
+        while True:
+            if queue[0] == numbers[i]:
+                queue.pop(0)
+                break
+            else:
+                queue.insert(0, queue.pop())
+                result += 1
+
+print(result)
