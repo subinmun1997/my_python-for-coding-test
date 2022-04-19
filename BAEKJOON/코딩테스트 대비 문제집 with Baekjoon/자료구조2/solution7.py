970f2d6033de0487db95d2ca376c7d1d087f5e6c
subinmun1997 <qzxy812@gmail.com>
2022-04-20 00:17:09 +0900

    N번째 큰 수

---
+++ BAEKJOON/코딩테스트 대비 문제집 with Baekjoon/자료구조2/solution7.py
@@ -0,0 +1,18 @@
+import heapq
+
+n = int(input())
+
+heap = []
+for _ in range(n):
+    nums = list(map(int, input().split()))
+
+    if not heap:
+        for num in nums:
+            heapq.heappush(heap, num)
+    else:
+        for num in nums:
+            if heap[0] < num:
+                heapq.heappush(heap, num)
+                heapq.heappop(heap)
+
+print(heap[0])
