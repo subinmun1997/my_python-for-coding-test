d07320463786c61caa7164ffd2c7e569842aefab
subinmun1997 <qzxy812@gmail.com>
2022-04-21 15:07:18 +0900

    키로거

---
+++ BAEKJOON/코딩테스트 대비 문제집 with Baekjoon/자료구조/solution22.py
@@ -0,0 +1,21 @@
+t = int(input())
+
+for _ in range(t):
+    left_stack = []
+    right_stack = []
+    data = input()
+    for i in data:
+        if i == "<":
+            if left_stack:
+                right_stack.append(left_stack.pop())
+        elif i == ">":
+            if right_stack:
+                left_stack.append(right_stack.pop())
+        elif i == "-":
+            if left_stack:
+                left_stack.pop()
+        else:
+            left_stack.append(i)
+
+    left_stack.extend(reversed(right_stack))
+    print("".join(left_stack))
