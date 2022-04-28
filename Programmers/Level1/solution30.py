f9fd94944f8a937334b6a61edba9560dd0f1c408
subinmun1997 <qzxy812@gmail.com>
2022-04-28 22:05:20 +0900

    3진법 뒤집기

---
+++ Programmers/Level1/solution30.py
@@ -0,0 +1,10 @@
+def solution(n):
+    array = ''
+    while n:
+        array += str((n%3))
+        n //= 3
+    array = int(array, 3)
+    return array
+
+print(solution(45))
+print(solution(125))
