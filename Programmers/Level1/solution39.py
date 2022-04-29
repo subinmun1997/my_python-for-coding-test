799c2f876d045473bcfe0f70a418b89e0b8ee58c
subinmun1997 <qzxy812@gmail.com>
2022-04-30 00:50:57 +0900

    같은 숫자는 싫어

---
+++ Programmers/Level1/solution39.py
@@ -0,0 +1,10 @@
+def solution(arr):
+    answer = []
+    answer.append(arr[0])
+    for i in range(1, len(arr)):
+        if arr[i-1] != arr[i]:
+            answer.append(arr[i])
+    return answer
+
+print(solution([1,1,3,3,0,1,1]))
+print(solution([4,4,4,3,3,]))
