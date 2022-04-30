74e28f4c0300ee4dfea628eec06c2af9a61daef6
subinmun1997 <qzxy812@gmail.com>
2022-05-01 00:54:12 +0900

    나누어 떨어지는 숫자 배열

---
+++ Programmers/Level1/solution40.py
@@ -0,0 +1,10 @@
+def solution(arr, divisor):
+    answer = []
+    for i in arr:
+        if i%divisor == 0:
+            answer.append(i)
+    return sorted(answer) if answer else [-1]
+
+print(solution([5,9,7,10], 5))
+print(solution([2,36,1,3], 1))
+print(solution([3,2,6], 10))
