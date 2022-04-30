a2d9f282dcc8439f134ca87b69bdd9ad74e862ce
subinmun1997 <qzxy812@gmail.com>
2022-05-01 01:14:17 +0900

    나누어 떨어지는 숫자 배열_간단한 풀이

--- Programmers/Level1/solution41.py
+++ Programmers/Level1/solution41.py
@@ -2,9 +2,6 @@ def solution(arr, divisor):
     answer = [i for i in arr if i%divisor == 0]
     return sorted(answer) if answer else [-1]
 
-
-
-
 print(solution([5,9,7,10], 5))
 print(solution([2,36,1,3], 1))
 print(solution([3,2,6], 10))
