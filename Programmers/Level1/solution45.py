4826bf1e6e84c59ad40a85a391ee397fb3933f78
subinmun1997 <qzxy812@gmail.com>
2022-05-02 18:45:47 +0900

    완주하지 못한 선수_Counter 풀이

---
+++ Programmers/Level1/solution45.py
@@ -0,0 +1,8 @@
+from collections import Counter
+
+def solution(participant, completion):
+    answer = Counter(participant) - Counter(completion)
+    return list(answer.keys())[0]
+
+print(solution(["leo","kiki","eden"], ["eden","kiki"]))
+print(solution(["mislav","stanko","mislav","ana"], ["stanko","ana","mislav"]))
