475e746eb6fc6c6a81f8e3ef06acf4d3c6071e66
subinmun1997 <qzxy812@gmail.com>
2022-05-10 15:44:31 +0900

    신고 결과 받기

---
+++ Programmers/Level1/solution65.py
@@ -0,0 +1,15 @@
+def solution(id_list, report, k):
+    answer = [0] * len(id_list)
+    reports = {x : 0 for x in id_list}
+
+    for r in set(report):
+        reports[r.split()[1]] += 1
+
+    for r in set(report):
+        if reports[r.split()[1]] >= k:
+            answer[id_list.index(r.split()[0])] += 1
+
+    return answer
+
+print(solution(["muzi", "frodo", "apeach", "neo"], ["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"], 2))
+print(solution(["con", "ryan"], ["ryan con", "ryan con", "ryan con", "ryan con"], 3))
