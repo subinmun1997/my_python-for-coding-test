e7d1144ce431350fe9ff5a9955929eb25924143e
subinmun1997 <qzxy812@gmail.com>
2022-05-21 20:58:28 +0900

    미아 노트

---
+++ BAEKJOON/코딩테스트 대비 문제집 with Baekjoon/문자열/solution38.py
@@ -0,0 +1,14 @@
+n, h, w = map(int, input().split())
+words = [input() for _ in range(h)]
+answer = ["?" for _ in range(n)]
+
+for word in words:
+    idx = 0
+    for i in range(n):
+        for char in word[idx:idx+w]:
+            if char != '?':
+                answer[i] = char
+
+        idx += w
+
+print(''.join(answer))
