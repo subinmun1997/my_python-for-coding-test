06c3ff8440bc341e3b9d9ab51f2ac08552bba05a
subinmun1997 <qzxy812@gmail.com>
2022-05-27 21:55:40 +0900

    유성

---
+++ BAEKJOON/코딩테스트 대비 문제집 with Baekjoon/구현/solution26.py
@@ -0,0 +1,20 @@
+r, s = map(int, input().split())
+photo = [list(input()) for _ in range(r)]
+
+min_height = r
+for row in range(s):
+    for col in range(r - 1, -1, -1):
+        if photo[col][row] == 'X':
+            height = 0
+            while photo[col + height + 1][row] == '.':
+                height += 1
+            min_height = min(min_height, height)
+            break
+
+for col in range(r - 1, -1, -1):
+    for row in range(s):
+        if photo[col][row] == 'X':
+            photo[col][row] = '.'
+            photo[col + min_height][row] = 'X'
+for col in photo:
+    print(''.join(col))
