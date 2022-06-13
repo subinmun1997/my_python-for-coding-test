a3b94b25dffba8dc161d249fc643f0710c01e2a9
subinmun1997 <qzxy812@gmail.com>
2022-06-13 12:09:19 +0900

    트리의 부모 찾기_dfs

---
+++ BAEKJOON/코딩테스트 대비 문제집 with Baekjoon/그래프 탐색/solution9.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(100000)
+
+n = int(input())
+graph = [[] for _ in range(n+1)]
+answer = [0] * (n+1)
+
+for _ in range(n-1):
+    x, y = map(int, input().split())
+    graph[x].append(y)
+    graph[y].append(x)
+
+def dfs(graph, v, visited):
+    visited[v] = True
+    for i in graph[v]:
+        if not visited[i]:
+            answer[i] = v
+            dfs(graph, i, visited)
+
+visited = [False] * (n+1)
+dfs(graph, 1, visited)
+for i in range(2, n+1):
+    print(answer[i])
