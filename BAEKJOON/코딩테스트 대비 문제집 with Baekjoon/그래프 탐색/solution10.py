11b12bfded38ec1e134e209bb7d46a00c1cc4ea5
subinmun1997 <qzxy812@gmail.com>
2022-06-13 12:25:19 +0900

    트리의 부모 찾기_bfs

---
+++ BAEKJOON/코딩테스트 대비 문제집 with Baekjoon/그래프 탐색/solution10.py
@@ -0,0 +1,26 @@
+from collections import deque
+
+n = int(input())
+graph = [[] for _ in range(n+1)]
+answer = [0] * (n+1)
+
+for _ in range(n-1):
+    x, y = map(int, input().split())
+    graph[x].append(y)
+    graph[y].append(x)
+
+visited = [False] * (n+1)
+def bfs(graph, start, visited):
+    queue = deque([start])
+    visited[start] = True
+    while queue:
+        v = queue.popleft()
+        for i in graph[v]:
+            if not visited[i]:
+                answer[i] = v
+                queue.append(i)
+                visited[i] = True
+
+bfs(graph, 1, visited)
+for i in range(2, n+1):
+    print(answer[i])
