1335c474384e538e7b8461bc00b24c2ec9dd943a
subinmun1997 <qzxy812@gmail.com>
2022-06-17 14:15:03 +0900

    소수 찾기

---
+++ Programmers/Level1_복습/solution13.py
@@ -0,0 +1,10 @@
+def solution(n):
+    num = set(range(2, n+1))
+    for i in range(2, int(n**0.5)+1):
+        if i in num:
+            num -= set(range(2*i, n+1, i))
+
+    return len(num)
+
+print(solution(10))
+print(solution(5))
