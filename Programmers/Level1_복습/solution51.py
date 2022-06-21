b5a3e8a930029bd30f742e228673195263568613
subinmun1997 <qzxy812@gmail.com>
2022-06-21 14:34:49 +0900

    완주하지 못한 선수_다른 풀이

---
+++ Programmers/Level1_복습/solution51.py
@@ -0,0 +1,10 @@
+from collections import Counter
+
+def solution(participant, completion):
+    answer = Counter(participant) - Counter(completion)
+    return list(answer.keys())[0]
+
+
+print(solution(["leo", "kiki", "eden"], ["eden", "kiki"]))
+print(solution(["marina", "josipa", "nikola", "vinko", "filipa"], ["josipa", "filipa", "marina", "nikola"]))
+print(solution(["mislav", "stanko", "mislav", "ana"], ["stanko", "ana", "mislav"]))
