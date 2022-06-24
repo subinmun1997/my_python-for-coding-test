6c4bdaaff983a546c4e3e013acb069deefc6c603
subinmun1997 <qzxy812@gmail.com>
2022-06-24 19:35:39 +0900

    숫자 문자열과 영단어

--- Programmers/Level1_복습/solution61.py
+++ Programmers/Level1_복습/solution61.py
@@ -1,11 +1,10 @@
 number = {'zero':"0", 'one':"1", 'two':"2", 'three':"3", 'four':"4", 'five':"5", 'six':"6", 'seven':"7", 'eight':"8", 'nine':"9"}
 
 def solution(s):
-    answer = s
     for key, value in number.items():
-        answer = answer.replace(key, value)
+        s = s.replace(key, value)
 
-    return answer
+    return int(s)
 
 
 print(solution("one4seveneight"))
