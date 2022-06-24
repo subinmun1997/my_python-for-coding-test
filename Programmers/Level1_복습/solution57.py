952612b7e30049c1ddc951a5aa37e6db41e4e461
subinmun1997 <qzxy812@gmail.com>
2022-06-24 18:01:01 +0900

    로또의 최고 순위와 최저 순위

---
+++ Programmers/Level1_복습/solution57.py
@@ -0,0 +1,20 @@
+def solution(lottos, win_nums):
+    min_cnt = 0
+    for lotto in lottos:
+        if lotto in win_nums:
+            min_cnt += 1
+    max_cnt = min_cnt + lottos.count(0)
+
+    max_score = 7 - max_cnt
+    if max_score > 6:
+        max_score = 6
+    min_score = 7 - min_cnt
+    if min_score > 6:
+        min_score = 6
+
+    return [max_score, min_score]
+
+
+print(solution([44, 1, 0, 0, 31, 25], [31, 10, 45, 1, 6, 19]))
+print(solution([0, 0, 0, 0, 0, 0], [38, 19, 20, 40, 15, 25]))
+print(solution([45, 4, 35, 20, 3, 9], [20, 9, 3, 45, 4, 35]))
