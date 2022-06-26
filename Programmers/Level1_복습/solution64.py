365a26d7427a0af6f086fc457a9db2835c2e1e96
subinmun1997 <qzxy812@gmail.com>
2022-06-26 18:59:18 +0900

    크레인 인형뽑기 게임

---
+++ Programmers/Level1_복습/solution64.py
@@ -0,0 +1,17 @@
+def solution(board, moves):
+    answer = 0
+    stack = []
+    for move in moves:
+        for i in range(len(board)):
+            if board[i][move-1] != 0:
+                stack.append(board[i][move-1])
+                board[i][move-1] = 0
+                break
+        if len(stack) >= 2 and stack[-1] == stack[-2]:
+            answer += 2
+            stack.pop()
+            stack.pop()
+
+    return answer
+
+print(solution([[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]], [1,5,3,5,1,2,1,4]))
