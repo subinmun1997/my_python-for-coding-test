587375a5328f66b4f8856643db954313915d822f
subinmun1997 <qzxy812@gmail.com>
2022-06-27 18:45:19 +0900

    DFS와 BFS

---
+++ BAEKJOON/코딩테스트 대비 문제집 with Baekjoon/그래프 탐색/solution17.py
@@ -0,0 +1,38 @@
+from collections import deque
+
+n, m, v = map(int, input().split())
+graph = [[] for _ in range(n+1)]
+
+for _ in range(m):
+    x, y = map(int, input().split())
+    graph[x].append(y)
+    graph[y].append(x)
+
+# 문제에서 정점 번호가 작은 것부터 방문한다는 것 구현 부분
+for j in range(n+1):
+    graph[j].sort()
+
+def dfs(graph, v, visited):
+    visited[v] = True
+    print(v, end=' ')
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
+
+def bfs(graph, start, visited):
+    visited[start] = True
+    queue = deque([start])
+    while queue:
+        v = queue.popleft()
+        print(v, end=' ')
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
+
+visited = [False] * (n+1)
+dfs(graph, v, visited)
+
+print()
+visited = [False] * (n+1)
+bfs(graph, v, visited)
