8410f27f8f0989c3a5f235312d24c3a8d79752d4
subinmun1997 <qzxy812@gmail.com>
2022-06-27 12:39:54 +0900

    행렬의 곱셈

---
+++ Programmers/Level2/solution15.py
@@ -0,0 +1,12 @@
+def solution(arr1, arr2):
+    answer = [[0 for col in range(len(arr2[0]))] for row in range(len(arr1))]
+    for i in range(len(arr1)):
+        for j in range(len(arr1[0])):
+            for k in range(len(arr2[0])):
+                answer[i][j] += (arr1[i][k] * arr2[k][j])
+
+    return answer
+
+
+print(solution([[1, 4], [3, 2], [4, 1]], [[3, 3], [3, 3]]))
+print(solution([[2, 3, 2], [4, 2, 4], [3, 1, 4]], [[5, 4, 3], [2, 4, 1], [3, 1, 1]]))
