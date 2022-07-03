8cce1f790182ea9bd4138f4f957c943875dd45fc
subinmun1997 <qzxy812@gmail.com>
2022-07-03 16:17:25 +0900

    하노이 탑

---
+++ BAEKJOON/solution671.py
@@ -0,0 +1,15 @@
+n = int(input())
+answer = []
+
+def hanoi(n, start, end, mid):
+    if n == 1:
+        print(start, end)
+        return
+    else:
+        hanoi(n-1, start, mid, end)
+        print(start, end)
+        hanoi(n-1, mid, end, start)
+
+print(2**n-1)
+if n <= 20:
+    hanoi(n, 1, 3, 2)
