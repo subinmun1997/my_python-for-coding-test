7e082a1944bf62c3f80b7d6302a2a6813df02324
subinmun1997 <qzxy812@gmail.com>
2022-07-03 19:29:03 +0900

    N개의 최소공배수

---
+++ Programmers/Level2/solution24.py
@@ -0,0 +1,13 @@
+import math
+from collections import deque
+
+def solution(arr):
+    queue = deque(arr)
+    while len(queue) != 1:
+        a = queue.popleft()
+        b = queue.popleft()
+        queue.appendleft(a*b // math.gcd(a, b))
+    return queue[0]
+
+print(solution([2, 6, 8, 14]))
+print(solution([1, 2, 3]))
