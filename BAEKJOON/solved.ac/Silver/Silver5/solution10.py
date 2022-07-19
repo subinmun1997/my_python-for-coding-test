62729d3bda52a68201bb41d260d143258a27a02d
subinmun1997 <qzxy812@gmail.com>
2022-07-19 17:22:12 +0900

    크로아티아 알파벳

---
+++ BAEKJOON/solved.ac/Silver/Silver5/solution10.py
@@ -0,0 +1,9 @@
+word = input()
+croatia = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
+
+count = 0
+for i in croatia:
+    if i in word:
+        word = word.replace(i, '0')
+
+print(len(word))
