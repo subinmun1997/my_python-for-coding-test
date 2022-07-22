5e3dfb0f952ec3fff2547440922d83481b7c7e14
subinmun1997 <qzxy812@gmail.com>
2022-07-22 10:50:23 +0900

    소수 구하기

---
+++ BAEKJOON/solved.ac/Silver/Silver3/solution4.py
@@ -0,0 +1,14 @@
+n, m = map(int, input().split())
+
+def prime(n):
+    if n == 1:
+        return False
+
+    for i in range(2, int(n**0.5)+1):
+        if n%i == 0:
+            return False
+    return True
+
+for i in range(n, m+1):
+    if prime(i):
+        print(i)
