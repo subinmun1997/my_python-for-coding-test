2cf96d6029628e962309471584038890113beaae
subinmun1997 <qzxy812@gmail.com>
2022-07-26 15:50:37 +0900

    유기농 배추

---
+++ BAEKJOON/solved.ac/Silver/Silver2/solution2.py
@@ -0,0 +1,31 @@
+import sys
+sys.setrecursionlimit(10000)
+
+t = int(input())
+
+def dfs(x, y):
+    if 0 <= x < n and 0 <= y < m:
+        if graph[x][y] == 1:
+            graph[x][y] = 0
+            dfs(x-1, y)
+            dfs(x+1, y)
+            dfs(x, y+1)
+            dfs(x, y-1)
+            return 1
+    return 0
+
+for _ in range(t):
+    m, n, k = map(int, input().split()) # m:가로, n:세로
+    graph = [[0 for _ in range(m)] for _ in range(n)]
+
+    for _ in range(k):
+        a, b = map(int, input().split())
+        graph[b][a] = 1
+
+    count = 0
+    for i in range(n):
+        for j in range(m):
+            if dfs(i, j) == 1:
+                count += 1
+
+    print(count)
