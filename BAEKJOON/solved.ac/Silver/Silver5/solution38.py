3c3450a1ac2ceee74bc70f6fc4e6c45a49321fb6
subinmun1997 <qzxy812@gmail.com>
2022-07-29 15:40:32 +0900

    캠핑

--- BAEKJOON/solved.ac/Silver/Silver5/solution38.py
+++ BAEKJOON/solved.ac/Silver/Silver5/solution38.py
@@ -3,7 +3,7 @@ while True:
     L, P, V = map(int, input().split())
 
     day += 1
-    if L == 0 and  P == 0 and V == 0:
+    if L == 0 and P == 0 and V == 0:
         break
     max_day = (V//P) * L + min(L, (V%P))
     print(f"Case {day}: {max_day}")
