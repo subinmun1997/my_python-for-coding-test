c72f8d8a9c199c3371a03abc0aa11366c5005173
subinmun1997 <qzxy812@gmail.com>
2022-08-02 20:48:06 +0900

    미로 탐색

---
+++ BAEKJOON/solved.ac/Silver/Silver1/solution1.py
@@ -0,0 +1,29 @@
+from collections import deque
+
+n, m = map(int, input().split())
+graph = [list(map(int, input())) for _ in range(n)]
+
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+def bfs(x, y):
+    queue = deque()
+    queue.append((x, y))
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if nx < 0 or nx >= n or ny < 0 or ny >= m:
+                continue
+            elif graph[nx][ny] == 0:
+                continue
+            elif graph[nx][ny] == 1:
+                graph[nx][ny] = graph[x][y] + 1
+                queue.append((nx, ny))
+
+    return graph[n-1][m-1]
+
+print(bfs(0, 0))
+
+    
