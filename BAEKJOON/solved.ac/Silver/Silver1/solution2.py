2c85a16b17dde6f2f518b6ab469ad962770feb69
subinmun1997 <qzxy812@gmail.com>
2022-08-03 18:30:24 +0900

    단지번호붙이기

---
+++ BAEKJOON/solved.ac/Silver/Silver1/solution2.py
@@ -0,0 +1,31 @@
+n = int(input())
+graph = [list(map(int, input())) for _ in range(n)]
+
+def dfs(x, y):
+    if x < 0 or x >= n or y < 0 or y >= n:
+        return False
+    if graph[x][y] == 1:
+        global cnt
+        cnt += 1
+        graph[x][y] = 0
+        dfs(x-1, y)
+        dfs(x+1, y)
+        dfs(x, y-1)
+        dfs(x, y+1)
+        return True
+    return False
+
+village = 0
+cnt = 0
+house = []
+for i in range(n):
+    for j in range(n):
+        if dfs(i, j):
+            house.append(cnt)
+            village += 1
+            cnt = 0
+
+print(village)
+house.sort()
+for i in house:
+    print(i)
