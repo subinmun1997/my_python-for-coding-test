908b967c8bb43dc0090cdf1c57a07409ca615ea1
subinmun1997 <qzxy812@gmail.com>
2022-08-06 23:14:20 +0900

    올림픽

---
+++ BAEKJOON/solved.ac/Silver/Silver5/solution44.py
@@ -0,0 +1,14 @@
+n, k = map(int, input().split())
+medals = [list(map(int, input().split())) for _ in range(n)]
+medals.sort(key=lambda x : (x[1], x[2], x[3]), reverse=True)
+
+for i in range(n):
+    if medals[i][0] == k:
+        idx = i
+
+for i in range(n):
+    if medals[idx][1:] == medals[i][1:]:
+        #print(medals[idx][1:])
+        #print(medals[i][1:])
+        print(i+1)
+        break
