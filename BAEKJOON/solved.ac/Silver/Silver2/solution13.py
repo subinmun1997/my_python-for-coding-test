3929adefdd282324ec8a56eef3d7f166caeedc7b
subinmun1997 <qzxy812@gmail.com>
2022-08-08 17:10:08 +0900

    골드바흐의 추측

---
+++ BAEKJOON/solved.ac/Silver/Silver2/solution13.py
@@ -0,0 +1,21 @@
+t = int(input())
+
+def is_prime(n):
+    if n == 1:
+        return False
+    for i in range(2, int(n**0.5)+1):
+        if n%i == 0:
+            return False
+    return True
+
+for _ in range(t):
+    n = int(input())
+
+    a, b = n//2, n//2
+    while a > 0:
+        if is_prime(a) and is_prime(b):
+            print(a, b)
+            break
+        else:
+            a -= 1
+            b += 1
