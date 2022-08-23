e9b3279657c53df55fdeac9ddabf01c2295a85d4
subinmun1997 <qzxy812@gmail.com>
2022-08-23 14:45:05 +0900

    영화감독 숌

---
+++ BAEKJOON/solved.ac/Class2/solution5.py
@@ -0,0 +1,11 @@
+n = int(input())
+
+cnt = 0
+movieName = 666
+while True:
+    if '666' in str(movieName):
+        cnt += 1
+    if cnt == n:
+        print(movieName)
+        break
+    movieName += 1
