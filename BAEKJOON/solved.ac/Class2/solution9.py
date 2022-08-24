81fe8a13c56763f964140b85e0e2dd20df207ce1
subinmun1997 <qzxy812@gmail.com>
2022-08-24 20:52:14 +0900

    소수 구하기

---
+++ BAEKJOON/solved.ac/Class2/solution9.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+def is_prime(n):
+    if n == 1:
+        return False
+    for i in range(2, int(n**0.5)+1):
+        if n%i == 0:
+            return False
+    return True
+
+for i in range(n, m+1):
+    if is_prime(i):
+        print(i)
