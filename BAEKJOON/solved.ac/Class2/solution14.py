c2e3276b19aabc3748c090beecaf6a6a38afe5bd
subinmun1997 <qzxy812@gmail.com>
2022-08-27 13:23:08 +0900

    카드2

---
+++ BAEKJOON/solved.ac/Class2/solution14.py
@@ -0,0 +1,13 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+n = int(input())
+queue = deque(i for i in range(1, n+1))
+
+while len(queue) != 1:
+    queue.popleft()
+    queue.append(queue.popleft())
+
+print(queue[0])
