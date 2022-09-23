6d8ec13243bee7080546c59f21570fed12b679ef
subinmun1997 <qzxy812@gmail.com>
2022-09-23 12:50:44 +0900

    최소 힙

---
+++ BAEKJOON/solved.ac/Class3/solution14.py
@@ -0,0 +1,15 @@
+import heapq, sys
+input = sys.stdin.readline
+
+n = int(input())
+
+heap = []
+for _ in range(n):
+    x = int(input())
+    if x == 0:
+        if heap:
+            print(heapq.heappop(heap))
+        else:
+            print(0)
+    else:
+        heapq.heappush(heap, x)
