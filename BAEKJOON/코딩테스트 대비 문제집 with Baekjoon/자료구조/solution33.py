407852440d4df3a5056783baa1e58f4f1c8eaf4d
subinmun1997 <qzxy812@gmail.com>
2022-10-22 11:40:07 +0900

    카드2

---
+++ BAEKJOON/코딩테스트 대비 문제집 with Baekjoon/자료구조/solution33.py
@@ -0,0 +1,10 @@
+from collections import deque
+
+n = int(input())
+card = deque([i for i in range(1, n+1)])
+
+while len(card) != 1:
+    card.popleft()
+    card.append(card.popleft())
+
+print(card[0])
