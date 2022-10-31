0f79c4387c6e806af2cbb700d613e448d9b7de03
subinmun1997 <qzxy812@gmail.com>
2022-10-31 16:25:19 +0900

    괄호의 값

---
+++ BAEKJOON/코딩테스트 대비 문제집 with Baekjoon/자료구조/solution40.py
@@ -0,0 +1,34 @@
+s = input()
+
+stack = []
+answer = 0
+temp = 1
+
+for i in range(len(s)):
+    if s[i] == '(':
+        stack.append(s[i])
+        temp *= 2
+    elif s[i] == '[':
+        stack.append(s[i])
+        temp *= 3
+    elif s[i] == ')':
+        if not stack or stack[-1] == '[':
+            answer = 0
+            break
+        if s[i-1] == '(':
+            answer += temp
+        stack.pop()
+        temp //= 2
+    else:
+        if not stack or stack[-1] == '(':
+            answer = 0
+            break
+        if s[i-1] == '[':
+            answer += temp
+        stack.pop()
+        temp //= 3
+
+if stack:
+    print(0)
+else:
+    print(answer)
