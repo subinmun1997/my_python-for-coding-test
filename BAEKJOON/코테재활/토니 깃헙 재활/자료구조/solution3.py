5255b0867407e2a6b580048fe6f56d5d18d3561d
subinmun1997 <qzxy812@gmail.com>
2022-11-19 13:55:01 +0900

    괄호

---
+++ BAEKJOON/코테재활/토니 깃헙 재활/자료구조/solution3.py
@@ -0,0 +1,12 @@
+t = int(input())
+
+for _ in range(t):
+    ps = input()
+
+    while '()' in ps:
+        ps = ps.replace('()', '')
+
+    if ps:
+        print("NO")
+    else:
+        print("YES")
