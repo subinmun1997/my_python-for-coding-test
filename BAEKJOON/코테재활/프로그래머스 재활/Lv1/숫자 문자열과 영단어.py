c3f4f0033765eaedf675aceec0069039541d6c8e
subinmun1997 <qzxy812@gmail.com>
2022-12-08 16:47:53 +0900

    숫자 문자열과 영단어

---
+++ BAEKJOON/코테재활/프로그래머스 재활/Lv1/숫자 문자열과 영단어.py
@@ -0,0 +1,11 @@
+def solution(s):
+    word = {"zero":"0", "one":"1", "two":"2", "three":"3", "four":"4", "five":"5", "six":"6", "seven":"7", "eight":"8", "nine":"9"}
+    for k, v in word.items():
+        s = s.replace(k, v)
+
+    return int(s)
+
+print(solution("one4seveneight"))
+print(solution("23four5six7"))
+print(solution("2three45sixseven"))
+print(solution("123"))
