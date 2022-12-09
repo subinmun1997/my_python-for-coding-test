01bc4ad4bc6afd7bc93711faf0c5a3639b6e3ed6
subinmun1997 <qzxy812@gmail.com>
2022-12-09 12:47:10 +0900

    2016년

---
+++ BAEKJOON/코테재활/프로그래머스 재활/Lv1/2016년.py
@@ -0,0 +1,7 @@
+def solution(a, b):
+    month = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    days = ["THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED"]
+
+    return days[(sum(month[:a-1]) + b) % 7]
+
+print(solution(5, 24))
