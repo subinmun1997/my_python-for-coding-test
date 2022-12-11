4d4d6a7f758d8ad7b9bfa77ebe176dc1ee44158e
subinmun1997 <qzxy812@gmail.com>
2022-12-11 15:20:35 +0900

    로또의 최고 순위와 최저 순위

---
+++ BAEKJOON/코테재활/프로그래머스 재활/Lv1/로또의 최고 순위와 최저 순위.py
@@ -0,0 +1,12 @@
+def solution(lottos, win_nums):
+    rank = [6,6,5,4,3,2,1]
+    cnt = 0
+    for i in win_nums:
+        if i in lottos:
+            cnt += 1
+
+    return rank[cnt + lottos.count(0)], rank[cnt]
+
+print(solution([44, 1, 0, 0, 31, 25], [31, 10, 45, 1, 6, 19]))
+print(solution([0, 0, 0, 0, 0, 0], [38, 19, 20, 40, 15, 25]))
+print(solution([45, 4, 35, 20, 3, 9], [20, 9, 3, 45, 4, 35]	))
