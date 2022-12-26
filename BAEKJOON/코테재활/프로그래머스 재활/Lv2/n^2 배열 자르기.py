ccad640a6063a572a17c38578834d7c7d58be43f
subinmun1997 <qzxy812@gmail.com>
2022-12-26 12:15:44 +0900

    n^2 배열 자르기

---
+++ BAEKJOON/코테재활/프로그래머스 재활/Lv2/n^2 배열 자르기.py
@@ -0,0 +1,13 @@
+def solution(n, left, right):
+    answer = []
+    for i in range(left, right+1):
+        answer.append(max(i//n, i%n)+1)
+
+    return answer
+
+print(solution(3, 2, 5))
+print(solution(4, 7, 14))
+
+# 1 2 3
+# 2 2 3
+# 3 3 3
