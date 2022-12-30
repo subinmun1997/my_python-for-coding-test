69e136f9ddfd65bff06e85b1cf29ba742af632d5
subinmun1997 <qzxy812@gmail.com>
2022-12-30 13:57:55 +0900

    2 x n 타일링

---
+++ BAEKJOON/코테재활/프로그래머스 재활/Lv2/2 x n 타일링.py
@@ -0,0 +1,11 @@
+def solution(n):
+    dp = [0] * 60001
+    dp[1] = 1
+    dp[2] = 2
+
+    for i in range(3, n+1):
+        dp[i] = dp[i-1] + dp[i-2]
+
+    return dp[n] % 1000000007
+
+print(solution(4))
