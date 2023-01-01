46b4cfe5bae23f2060c6f273055ef42835a2ba0b
subinmun1997 <qzxy812@gmail.com>
2023-01-01 13:51:49 +0900

    124 나라의 숫자

---
+++ BAEKJOON/코테재활/프로그래머스 재활/Lv2/124 나라의 숫자.py
@@ -0,0 +1,14 @@
+def solution(n):
+    answer = ''
+    nums = ['1', '2', '4']
+
+    while n > 0:
+        n -= 1
+        answer = nums[n % 3] + answer
+        n //= 3
+    return answer
+
+print(solution(1))
+print(solution(2))
+print(solution(3))
+print(solution(4))
