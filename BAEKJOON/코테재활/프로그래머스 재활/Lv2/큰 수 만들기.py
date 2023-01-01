7b77e3fa3b788ce829a47baf5669bca872d4528d
subinmun1997 <qzxy812@gmail.com>
2023-01-01 15:36:05 +0900

    큰 수 만들기

---
+++ BAEKJOON/코테재활/프로그래머스 재활/Lv2/큰 수 만들기.py
@@ -0,0 +1,16 @@
+def solution(number, k):
+    stack = []
+    for num in number:
+        while stack and stack[-1] < num and k > 0:
+            k -= 1
+            stack.pop()
+        stack.append(num)
+
+    if k != 0:
+        stack = stack[:-k]
+
+    return ''.join(stack)
+
+print(solution("1924", 2))
+print(solution("1231234", 3))
+print(solution("4177252841", 4))
