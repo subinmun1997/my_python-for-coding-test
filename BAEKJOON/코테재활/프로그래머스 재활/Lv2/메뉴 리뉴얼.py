d22be4afabee0c394b7339e99e5ea8f366314957
subinmun1997 <qzxy812@gmail.com>
2023-01-02 20:12:52 +0900

    메뉴 리뉴얼

---
+++ BAEKJOON/코테재활/프로그래머스 재활/Lv2/메뉴 리뉴얼.py
@@ -0,0 +1,20 @@
+from itertools import combinations
+from collections import Counter
+
+def solution(orders, course):
+    answer = []
+    for c in course:
+        temp = []
+        for order in orders:
+            comb = combinations(list(sorted(order)), c)
+            temp += comb
+        counter = Counter(temp)
+        if len(counter) != 0 and max(counter.values()) != 1:
+            answer += [''.join(f) for f in counter if counter[f] == max(counter.values())]
+
+    return sorted(answer)
+
+
+print(solution(["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"], [2,3,4]))
+print(solution(["ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD"], [2,3,5]))
+print(solution(["XYZ", "XWY", "WXA"], [2,3,4]))
