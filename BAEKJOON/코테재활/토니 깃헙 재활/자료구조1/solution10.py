79ffd56d7630a3f3eb14f6aa6fddacd0ea95c5d2
subinmun1997 <qzxy812@gmail.com>
2023-01-05 22:16:36 +0900

    쇠막대기

---
+++ BAEKJOON/코테재활/토니 깃헙 재활/자료구조1/solution10.py
@@ -0,0 +1,17 @@
+laser = input()
+
+count = 0
+stack = []
+for i in range(len(laser)):
+    if laser[i] == '(':
+        stack.append(laser[i])
+    else:
+        if laser[i-1] == '(':
+            stack.pop()
+            count += len(stack)
+        else:
+            stack.pop()
+            count += 1
+
+print(count)
+
