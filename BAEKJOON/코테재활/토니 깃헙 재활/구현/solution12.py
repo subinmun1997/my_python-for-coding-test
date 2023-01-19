a9392f473ce0016695651bdd2768bddc0a5af34a
subinmun1997 <qzxy812@gmail.com>
2023-01-19 20:07:30 +0900

    퇴사

---
+++ BAEKJOON/코테재활/토니 깃헙 재활/구현/solution12.py
@@ -0,0 +1,11 @@
+n = int(input())
+schedule = [list(map(int, input().split())) for _ in range(n)]
+
+dp = [0] * (n+1)
+for i in range(n-1, -1, -1):
+    if i + schedule[i][0] > n:
+        dp[i] = dp[i+1]
+    else:
+        dp[i] = max(schedule[i][1] + dp[i + schedule[i][0]], dp[i+1])
+
+print(max(dp))
