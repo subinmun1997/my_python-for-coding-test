70fcfa2c5d7e99b6e13ef578607e3dae18825bed
subinmun1997 <qzxy812@gmail.com>
2023-01-21 16:14:24 +0900

    파일 정리

---
+++ BAEKJOON/코테재활/토니 깃헙 재활/구현/solution17.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+extension = dict()
+for _ in range(n):
+    name, ext = input().split('.')
+    if ext in extension:
+        extension[ext] += 1
+    else:
+        extension[ext] = 1
+
+result = sorted(extension.items(), key=lambda x : x[0])
+
+for x, y in result:
+    print(x, y)
