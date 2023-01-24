e35b8a1a22f3848fc6864c3be997715e11176156
subinmun1997 <qzxy812@gmail.com>
2023-01-24 22:38:51 +0900

    DFS와 BFS

---
+++ BAEKJOON/코테재활/토니 깃헙 재활/그래프 탐색/solution3.py
@@ -0,0 +1,38 @@
+from collections import deque
+
+n, m, v = map(int, input().split())
+graph = [[] for _ in range(n+1)]
+
+for _ in range(m):
+    x, y = map(int, input().split())
+    graph[x].append(y)
+    graph[y].append(x)
+
+for i in range(1, n+1):
+    graph[i].sort()
+
+def dfs(graph, v, visited):
+    visited[v] = True
+    print(v, end=' ')
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
+
+def bfs(graph, start, visited):
+    queue = deque([start])
+    visited[start] = True
+
+    while queue:
+        v = queue.popleft()
+        print(v, end=' ')
+        for i in graph[v]:
+            if not visited[i]:
+                visited[i] = True
+                queue.append(i)
+
+visited = [False] * (n+1)
+dfs(graph, v, visited)
+print()
+
+visited = [False] * (n+1)
+bfs(graph, v, visited)
