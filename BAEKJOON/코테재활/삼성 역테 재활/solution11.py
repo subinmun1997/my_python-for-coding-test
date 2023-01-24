a9431d3f5bf0e722d541a1334d1a41572951b798
subinmun1997 <qzxy812@gmail.com>
2023-01-24 17:42:44 +0900

    스타트와 링크

--- BAEKJOON/코테재활/삼성 역테 재활/solution11.py
+++ BAEKJOON/코테재활/삼성 역테 재활/solution11.py
@@ -1,64 +1,22 @@
-def turn45(n, d, graph):
-    count = abs(d) // 45
-    minus = False
-    if d < 0:
-        minus = True
+from itertools import combinations
 
-    for _ in range(count):
-        if not minus:
-            prev_list = []
+n = int(input())
+s = [list(map(int, input().split())) for _ in range(n)]
+members = [i for i in range(n)]
 
-            for i in range(n): #주 대각선
-                prev_list.append(graph[i][i])
+result = int(1e9)
+for r1 in combinations(members, n//2):
+    start, link = 0, 0
+    r2 = list(set(members) - set(r1))
 
-            for i in range(n): #주 대각선 > 가운데열
-                prev_temp = graph[i][n//2]
-                graph[i][n//2] = prev_list[i]
-                prev_list[i] = prev_temp
+    for r in combinations(r1, 2):
+        start += s[r[0]][r[1]]
+        start += s[r[1]][r[0]]
 
-            for i in range(n): #가운데열 > 부 대각선
-                prev_temp = graph[i][n-1-i]
-                graph[i][n-1-i] = prev_list[i]
-                prev_list[i] = prev_temp
+    for r in combinations(r2, 2):
+        link += s[r[0]][r[1]]
+        link += s[r[1]][r[0]]
 
-            for i in range(n): #부 대각선 > 가운데행
-                prev_temp = graph[n//2][n-1-i]
-                graph[n//2][n-1-i] = prev_list[i]
-                prev_list[i] = prev_temp
+    result = min(result, abs(start - link))
 
-            for i in range(n): #가운데 행 > 주대각선
-                graph[n-1-i][n-1-i] = prev_list[i]
-        else:
-            prev_list = []
-
-            for i in range(n): #주 대각선
-                prev_list.append(graph[i][i])
-
-            for i in range(n): #주 대각선 > 가운데 행
-                prev_temp = graph[n//2][i]
-                graph[n//2][i] = prev_list[i]
-                prev_list[i] = prev_temp
-
-            for i in range(n): #가운데 행 > 부 대각선
-                prev_temp = graph[n-1-i][i]
-                graph[n-1-i][i] = prev_list[i]
-                prev_list[i] = prev_temp
-
-            for i in range(n): #부 대각선 > 가운데 열
-                prev_temp = graph[n-1-i][n//2]
-                graph[n-1-i][n//2] = prev_list[i]
-                prev_list[i] = prev_temp
-
-            for i in range(n): #가운데 열 > 주 대각선
-                graph[n-1-i][n-1-i] = prev_list[i]
-
-t = int(input())
-for _ in range(t):
-    n, d = map(int, input().split())
-    graph = [list(map(int, input().split())) for _ in range(n)]
-    turn45(n, d, graph)
-
-    for i in range(n):
-        for j in range(n):
-            print(graph[i][j], end=' ')
-        print()
+print(result)
