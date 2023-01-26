6c8c56ad0cda5ff5d53e2bd001f4217eb227268c
subinmun1997 <qzxy812@gmail.com>
2023-01-26 16:44:46 +0900

    경사로

--- BAEKJOON/코테재활/삼성 역테 재활/solution12.py
+++ BAEKJOON/코테재활/삼성 역테 재활/solution12.py
@@ -14,12 +14,14 @@ def pos(now):
         if now[j] < now[j-1]: # 2. 현재 < 이전; 경사로를 만들기 위해 오른쪽 스캔
             for k in range(l): # l 만큼 경사로 너비 필요
                 if j+k >= n or used[j+k] or now[j] != now[j+k]: # 범위 넘어감 or 이미 설치함 or 낮은 곳의 높이가 다른 경우 false
+                    # 여기서 j+k >= n 부터 따져봐야되는 이유는 범위를 넘어간 상태로 used[j+k]나 now[j+k]를 먼저 따져보게 되면 index out of range error 발생하기 때문
                     return False
                 if now[j] == now[j+k]: # 높이가 같은 경우 사용 여부 체크
                     used[j+k] = True
         elif now[j] > now[j-1]: # 3. 현재 > 이전; 경사로를 만들기 위해 왼쪽 스캔
             for k in range(l):
                 if j-1-k < 0 or now[j-1] != now[j-1-k] or used[j-1-k]: # 범위 넘어감 or 낮은 곳의 높이가 다름 or 이미 설치함 false
+                    # # 여기서 j-1-k < 0 부터 따져봐야되는 이유는 범위를 넘어간 상태로 used[j+k]나 now[j+k]를 먼저 따져보게 되면 index out of range error 발생하기 때문
                     return False
                 if now[j-1] == now[j-1-k]: # 높이가 같은 경우 사용 여부 체크
                     used[j-1-k]
