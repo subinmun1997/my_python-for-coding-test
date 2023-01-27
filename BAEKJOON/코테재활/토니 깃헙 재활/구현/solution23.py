3ca5f2a504c1756f4fc12f8b5c3edffb12b68736
subinmun1997 <qzxy812@gmail.com>
2023-01-27 11:42:00 +0900

    배열 돌리기1

---
+++ BAEKJOON/코테재활/토니 깃헙 재활/구현/solution23.py
@@ -0,0 +1,42 @@
+import sys
+input = sys.stdin.readline
+
+n, m, r = map(int, input().split())
+data = [list(map(int, input().split())) for _ in range(n)]
+
+for _ in range(r):
+    for i in range(min(n, m) // 2):
+        x, y = i, i
+        temp = data[x][y]
+
+        for j in range(i+1, n-i):
+            x = j
+            prev_value = data[x][y]
+            data[x][y] = temp
+            temp = prev_value
+
+        # x=3, temp=5
+        for j in range(i+1, m-i):
+            y = j
+            prev_value = data[x][y]
+            data[x][y] = temp
+            temp = prev_value
+
+        #y=3, temp=2
+        for j in range(i+1, n-i):
+            x = n-j-1
+            prev_value = data[x][y]
+            data[x][y] = temp
+            temp = prev_value
+
+        # x=0, temp=4
+        for j in range(i+1, m-i):
+            y = m-j-1
+            prev_value = data[x][y]
+            data[x][y] = temp
+            temp = prev_value
+
+for i in range(n):
+    for j in range(m):
+        print(data[i][j], end=' ')
+    print()
