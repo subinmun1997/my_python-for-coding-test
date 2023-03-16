87d046daa0a02dc44ab1d555baee08b7988b1893
subinmun1997 <qzxy812@gmail.com>
2023-03-16 13:18:07 +0900

    바이러스-bfs

---
+++ BAEKJOON/백준 문제집/DFSBFS 필수 문제/solution13.py
@@ -0,0 +1,30 @@
+from collections import deque
+
+def bfs(graph, start, visited):
+    global count
+
+    queue = deque([start])
+    visited[start] = True
+
+    while queue:
+        v = queue.popleft()
+        for i in graph[v]:
+            if not visited[i]:
+                count += 1
+                visited[i] = True
+                queue.append(i)
+
+n = int(input())
+m = int(input())
+
+graph = [[] for _ in range(n+1)]
+for _ in range(m):
+    x, y = map(int, input().split())
+    graph[x].append(y)
+    graph[y].append(x)
+
+count = 0
+visited = [False] * (n+1)
+bfs(graph, 1, visited)
+
+print(count)
